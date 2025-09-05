a0b15228851502a0875ce534a2f80907e6956ee0
Tanay Agrawal <agrawaltanay21@gmail.com>
2025-09-05 05:51:18 +0000

    learning JavaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code asnewer version
+
+//alert(3+3)  //we are using node.js, not browser
+
+console.log(3
+    +
+    3) //code readiability should be high
+
+console.log("Tanay")
+
+let name  = "Tanay"
+let age  = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0 
+// undefined => NaN
+
+
+let isLoggedIn = "Tanay"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+//1 => true; 0 => false
+// "" => false
+// "Tanay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
