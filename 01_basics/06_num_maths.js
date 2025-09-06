16bce0c26be3012bc2baefa97d02903ae992191a
Tanay Agrawal <agrawaltanay21@gmail.com>
2025-09-06 08:09:03 +0000

    nums and maths

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,44 @@
+const score = 100
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));  // for decimal takes two places
+
+const otherNumber = 23.9648
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++ maths ++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));   //negative to positive
+// console.log(Math.round(4.6));  //roundOff
+
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+Math.max();  //to find maximum value
+Math.min();  //to find minimum value
+
+
+console.log(Math.random()); //lies between 0 and 1
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+  
+
+
+
+ 
+
+
